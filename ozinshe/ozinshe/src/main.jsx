77bb2aab03cefbc685e14edcd47fd10ae2668d49
Your Name <notbot817@gmail.com>
2025-04-25 17:27:23 +0500

    lyklyk

--- ozinshe/ozinshe/src/main.jsx
+++ ozinshe/ozinshe/src/main.jsx
@@ -8,13 +8,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <SpeedInsights
-        // projectId="speed-insights"
-        // clientId="speed-insights"
-        // clientSecret="speed-insights"
-      >
-        <App />
-      </SpeedInsights>
+    <SpeedInsights/>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
