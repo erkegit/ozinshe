7c814ea715bb65367b7c2397bb2734485d0d8982
Your Name <notbot817@gmail.com>
2025-04-25 17:21:53 +0500

    step for SI

--- ozinshe/ozinshe/src/main.jsx
+++ ozinshe/ozinshe/src/main.jsx
@@ -8,7 +8,13 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SpeedInsights
+        // projectId="speed-insights"
+        // clientId="speed-insights"
+        // clientSecret="speed-insights"
+      >
+        <App />
+      </SpeedInsights>
     </BrowserRouter>
   </StrictMode>,
 )
